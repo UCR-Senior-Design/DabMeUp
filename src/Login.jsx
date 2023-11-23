b5769d956c1c383b65b1c1c8661b73988e42a5d8
Alan <alanxuzhang@yahoo.com>
2023-11-22 22:01:07 -0800

    Added Authentication Functionality

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,29 +1,58 @@
-import React from "react";
-import "./Login.css"
+import React, { useState } from "react";
+import "./Login.css";
 
-const Login = () =>{
-    return(
-<div>
-    <div className=" container>">
+const Login = ({ onLogin, onSignUp }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // You can perform any additional validation here before calling onLogin
+    onLogin(email, password);
+  };
+
+  const handleSignUp = () => {
+    // You can perform any additional validation here before calling onSignUp
+    onSignUp(email, password);
+  };
+
+  return (
+    <div>
+      <div className="container">
         <div className="header">
+          <div className="input">
+            <h2>I am a login</h2>
+            <label htmlFor="email"><b>Email</b></label>
+            <input
+              type="email"
+              placeholder="enter email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
 
-        <div className="input">
-    <h2>I am a login</h2>
-    <label for="email"><b>Email</b></label>
-    <input type = "email" placeholder = "enter rmail" name = "rmail" required></input>
-    
-    <label for="password"><b>Password</b></label>
-    <input type = "password" placeholder = "enter password" name = "rmail" required></input>
-    <button type="enter" class="loginbtn">Log In</button>
+            <label htmlFor="password"><b>Password</b></label>
+            <input
+              type="password"
+              placeholder="enter password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
 
-    </div>
+            <button type="button" className="loginbtn" onClick={handleLogin}>
+              Log In
+            </button>
+
+            <button type="button" className="signupbtn" onClick={handleSignUp}>
+              Sign Up
+            </button>
+          </div>
         </div>
+      </div>
     </div>
-    
-</div>
-    )
-}
-
-
+  );
+};
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -7,20 +7,64 @@ import { Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Login from './Login';
 
+import { initializeApp } from "firebase/app";
+import {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyodiB5e0ZwjG6MaD3eT1MdYKJIUU4ql0",
+  authDomain: "palz-53350.firebaseapp.com",
+  projectId: "palz-53350",
+  storageBucket: "palz-53350.appspot.com",
+  messagingSenderId: "605826956457",
+  appId: "1:605826956457:web:b8c307be733921c8287db8",
+  measurementId: "G-KQ8L33TDQD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  /*
-  <React.StrictMode>
-    <Header />
-  </React.StrictMode>
-  */
- <Login />
 
-);
+const handleLogin = async (email, password) => {
+  try {
+    // Use Firebase signInWithEmailAndPassword function
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    
+    // You can access the user from the userCredential if needed
+    const user = userCredential.user;
+    
+    console.log("Successfully signed in:", user);
+    
+    // Add any additional logic you need after successful login
+  } catch (error) {
+    // Handle errors, e.g., display an error message to the user
+    console.error("Error signing in:", error.message);
+  }
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const handleSignUp = async (email, password) => {
+  try {
+    // Use Firebase createUserWithEmailAndPassword function
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+    // You can access the user from the userCredential if needed
+    const user = userCredential.user;
+
+    console.log("Successfully signed up:", user);
+
+    // Add any additional logic you need after successful sign-up
+  } catch (error) {
+    // Handle errors, e.g., display an error message to the user
+    console.error("Error signing up:", error.message);
+  }
+};
+
+root.render(<Login onLogin={handleLogin} onSignUp={handleSignUp} />);
 reportWebVitals();
