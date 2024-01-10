4ca9e2a27009ae685c6e5ff59b31e9cfbd64c769
Bashar Elasmar <enduser@ucr-secure-20-10-13-128-172.wnet.ucr.edu>
2024-01-09 20:20:15 -0800

    home page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React from 'react'
-import "./App.css";
-import Header from './Header';
-function App() {
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
+import CreateAccount from "./pages/CreateAccount"
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+const App = () => {
   return (
-    <div className="App">
-    <h1>Let's build the Palz app</h1>
-    
-    {/* Header*/}
-    <Header />
-    {/* Cards*/}
-    {/* Cards*/}
-    {/* buttons below cards*/}
-    {/* chat screen*/}
-    {/* individual chat screen*/}
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path = {""} element = {<Home/>}/>
+        <Route path = {"/dashboard"} element = {<Dashboard/>}/>
+        <Route path = {"/CreateAccount"} element = {<CreateAccount/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
-export default App;
- 
+
+export default App 
