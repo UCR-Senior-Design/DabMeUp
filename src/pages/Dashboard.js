81516afc52af3522ca7102511e4f627e7918a48f
Joshuaphak <jphak002@ucr.edu>
2024-02-05 21:39:41 -0800

    nav bar and cards

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,66 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import ChatContainer from './ChatContainer';
+import TinderCard from 'react-tinder-card';
+import { useState  } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 const Dashboard = () => {
   let navigate = useNavigate();
 
+  const characters = [
+    {
+      name: 'card 1'
+    },
+    {
+    name: 'card 2'
+
+    } 
+  ]
+
+const[lastDirection, setLastDirection] = useState() 
+
+  const swiped = (direction, swipedUserId) => {
+    if (direction === 'right') {
+        // updateMatches(swipedUserId)
+    }
+    setLastDirection(direction)
+}
+
+const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+}
+
+
   return (
-    <div  className= "overlay">
-      <h1>Dashboard</h1>
-      <button onClick={() => navigate('/Profile')}>Profile</button>
-      <button onClick={() => navigate('/Settings')}>Settings</button>
+    <div  className= "dashboard">
+      <div className='navbar'>
+      <button className='profbtn' onClick={() => navigate('/Profile')}>Profile</button>
+     
+      <button className='settbtn' onClick={() => navigate('/Settings')}>Settings</button>
       {/* Other dashboard content */}
+      </div>
+      <h1>Palz</h1>
+      
+      <ChatContainer/>
+     <div className='swipe-container'>
+      <div className='cardcontainer'>
+
+      {characters.map((character) => 
+        <TinderCard className='swipe' key = {character.name} onSwipe = {(dir) => swiped(dir, character.name)} onCardLeftScreen = {() => outOfFrame(character.name)}>
+          <div
+                                    // style={{backgroundImage: "url(" + genderedUser.url + ")"}}
+                                    // style={{backgroundImage: "url"}}
+                                    className="card">
+                                    {/* <h3>{genderedUser.first_name}</h3> */}
+                                    <h3>{"cards"}</h3>
+                                </div>
+        </TinderCard>
+      
+     )}
+      </div>
+      </div>
+      
     </div>
   );
 };
