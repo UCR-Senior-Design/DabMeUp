82554ca5007f83c684d8bccda383176d7e8fcf63
Bashar Elasmar <enduser@MacBook-Pro-12.home>
2024-02-05 21:27:08 -0800

    chat container created

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,18 +1,19 @@
-
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import ChatContainer from "../components/ChatContainer";
 
 const Dashboard = () => {
   let navigate = useNavigate();
 
   return (
-    <div  className= "overlay">
+    <div className="overlay">
       <h1>Dashboard</h1>
       <button onClick={() => navigate('/Profile')}>Profile</button>
       <button onClick={() => navigate('/Settings')}>Settings</button>
+      <ChatContainer /> {/* Include ChatContainer */}
       {/* Other dashboard content */}
     </div>
   );
 };
 
-
+export default Dashboard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,9 @@
 import Nav from '../components/Nav'
-import { useNavigate } from "react-router-dom"; 
-
 const Home = () => {
-    let navigate = useNavigate();
     const authToken = false
     const handeClick = () => {
-        if (authToken) {
-            navigate('/Dashboard'); // If user is authenticated, navigate to Dashboard
-        } else {
-            navigate('/Login'); // If not authenticated, navigate to Login
-        }
+        console.log('clicked')
     }
-    
     return (
         <div className= "overlay">
         <Nav minimal={false} authToken = {authToken}/>
@@ -25,27 +17,6 @@ const Home = () => {
         </div>
 
     )
-    /*
-    return (
-        <div className= "overlay">
-          <Nav minimal={false} authToken={authToken}/>
-          <div className="home">
-            <h1>Click Right</h1>
-            {authToken ? (
-              <button className="primary-button" onClick={(handeClick) => navigate('/Dashboard')}>
-                Go to Dashboard
-              </button>
-            ) : (
-              <>
-                <button className="primary-button" onClick={() => navigate('/CreateAccount')}>
-                  Create Account
-                </button>
-
-              </>
-            )}
-          </div>
-        </div>
-      )*/
 }
 
-export default Home 
+export default Home
